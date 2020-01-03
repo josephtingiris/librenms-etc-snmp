b234785ad7b73aded7a7986497e866e809fe864b
Joseph Tingiris <joseph.tingiris@gmail.com>
2020-01-03 15:09:08 -0500

    extend-example.sh: an example/template

---
+++ librenms/extend-example.sh
@@ -0,0 +1,61 @@
+#!/usr/bin/env bash
+
+# An example (and a template) for librenms-etc-snmp.
+
+# Dependencies:
+# bash
+
+# Copyright (C) 2020 Joseph Tingiris (joseph.tingiris@gmail.com)
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+#
+# 20200103, joseph.tingiris@gmail.com, created
+#
+
+PATH=/sbin:/bin:/usr/sbin:/usr/bin
+
+#
+# Functions
+#
+
+# for more, see extend.include.sh
+
+#
+# Globals
+#
+
+Basename=${0##*/}
+Dirname=${0%/*}
+
+#
+# Main
+#
+
+# source extend-include.sh or exit
+if [ -r "${Dirname}/extend-include.sh" ]; then
+    source "${Dirname}/extend-include.sh"
+else
+    exit 1
+fi
+
+debugecho "Basename=${Basename}" 1
+debugecho "Dirname=${Dirname}" 2
+
+cleanup "${Tmp_File}"
+
+error an error message goes to stderr
+
+aborting an aborting messages goes to stderr and immediately exits non-zero
+
