8a7569c9e1a5efa0738a5733d9122c6e53180d3f
Joseph Tingiris <joseph.tingiris@gmail.com>
2020-02-03 18:53:32 -0500

    Output setup-snmpd version

--- librenms/extend-setup-snmpd.sh
+++ librenms/extend-setup-snmpd.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# This script runs /etc/snmp/librenms scripts and adds extend entries to snmpd.conf.
+# Output setup-snmpd version
 
 # Copyright (C) 2020 Joseph Tingiris (joseph.tingiris@gmail.com)
 
@@ -29,8 +29,8 @@
 #
 
 function usage() {
-    printf "\nusage: $0 <check|install>\n\n"
-    exit 1
+    printf "\nusage: $0\n\n"
+    exit 2
 }
 
 # for more, see extend.include.sh
@@ -50,7 +50,6 @@ Dirname=${0%/*}
 if [ -r "${Dirname}/extend-include.sh" ]; then
     source "${Dirname}/extend-include.sh"
 else
-    "aborting .. can't source extend-include.sh"
     exit 1
 fi
 
@@ -58,117 +57,12 @@ if [ -r "${Extend_Include_Env}" ]; then
     source "${Extend_Include_Env}"
 fi
 
-if [ "$1" == "install" ]; then
-    Install=0 # true
-else
-    if [ "$1" == "check" ]; then
-        Install=1 # false
-    else
-        usage
-    fi
+echo $PWD
+pwd
+if [ -r ../setup-snmpd.version ]; then
+    cat ../setup-snmpd.version
+    exit 0
 fi
+echo did not find version
 
-if [ ${#Snmpd_Conf} -eq 0 ]; then
-    Snmpd_Conf=/etc/snmp/snmpd.conf
-fi
-
-if [ ! -w "${Snmpd_Conf}" ]; then
-    aborting "${Snmpd_Conf} file not writable"
-fi
-
-debugecho "Snmpd_Conf = ${Snmpd_Conf}" 5
-
-[ ${#Snmpd_Conf_Dir} -eq 0 ] && Snmpd_Conf_Dir=${Snmpd_Conf%/*}
-debugecho "Snmpd_Conf_Dir = ${Snmpd_Conf_Dir}" 5
-
-if [ ! -d "${Snmpd_Conf_Dir}" ]; then
-    aborting "${Snmpd_Conf_Dir} not found"
-fi
-
-[ ${#Snmpd_Conf_Extend_Dir} -eq 0 ] && Snmpd_Conf_Extend_Dir="${Snmpd_Conf_Dir}/librenms"
-
-if [ ! -d "${Snmpd_Conf_Extend_Dir}" ]; then
-    aborting "${Snmpd_Conf_Extend_Dir} not found"
-fi
-
-
-if [ -x "${Dirname}"/extend-info.sh ]; then
-    for Extend_Name in distro hardware manufacturer serial; do
-        "${Dirname}"/extend-info.sh "${Extend_Name}" &> /dev/null
-        Extend_RC=$?
-        if [ ${Extend_RC} -eq 0 ]; then
-            if [ "${Extend_Name}" == "distro" ]; then
-                Extend_OID=".1.3.6.1.4.1.2021.7890.1"
-            fi
-
-            if [ "${Extend_Name}" == "hardware" ]; then
-                Extend_OID=".1.3.6.1.4.1.2021.7890.2"
-            fi
-
-            if [ "${Extend_Name}" == "manufacturer" ]; then
-                Extend_OID=".1.3.6.1.4.1.2021.7890.3"
-            fi
-
-            if [ "${Extend_Name}" == "serial" ]; then
-                Extend_OID=".1.3.6.1.4.1.2021.7890.4"
-            fi
-
-            if [ ${#Extend_OID} -gt 0 ]; then
-                if [ ${Install} -eq 0 ]; then
-                    sed -Ei "/extend(.*)${Extend_OID}[[:space:]]/d" "${Snmpd_Conf}"
-                    if [ $? -eq 0 ]; then
-                        echo "extend ${Extend_OID} ${Extend_Name} '${Snmpd_Conf_Extend_Dir}/extend-info.sh ${Extend_Name}'" >> "${Snmpd_Conf}"
-                        if [ $? -eq 0 ]; then
-                            echo "+ extend ${Extend_OID} ${Extend_Name} installed."
-                        else
-                            echo "+ extend ${Extend_OID} ${Extend_Name} failed to install."
-                        fi
-                    fi
-                else
-                    echo "+ extend ${Extend_OID} ${Extend_Name} returns success."
-                fi
-            fi
-        fi
-        unset -v Extend_OID
-    done
-fi
-unset -v Extend_Name Extend_RC
-
-while read Extend_Check; do
-    Extend_Basename=${Extend_Check##*/}
-    Extend_Exec="${Snmpd_Conf_Extend_Dir}/${Extend_Basename}"
-    Extend_Name=${Extend_Basename}
-    Extend_Name=${Extend_Name//.bash/}
-    Extend_Name=${Extend_Name//.sh/}
-    Extend_Name=${Extend_Name//.php/}
-    Extend_Name=${Extend_Name//.pl/}
-    Extend_Conf="${Snmpd_Conf_Extend_Dir}/${Extend_Name}.conf"
-    Extend_Name=${Extend_Name/extend-/}
-    Extend_Name=${Extend_Name/custom-/}
-
-    if [ -r "${Extend_Conf}" ]; then
-        Extend_Exec_Args=" -c ${Extend_Conf}"
-    fi
-
-    debugecho "Extend_Check = ${Extend_Check}${Extend_Exec_Args} (${Extend_Basename}) [${Extend_Name}]" 10
-
-    ${Extend_Check}${Extend_Exec_Args} &> /dev/null
-    Extend_RC=$?
-    if [ ${Extend_RC} -eq 0 ]; then
-        if [ ${Install} -eq 0 ]; then
-            sed -Ei "/extend(.*)${Extend_Name}[[:space:]]/d" "${Snmpd_Conf}"
-            if [ $? -eq 0 ]; then
-                echo "extend ${Extend_Name} '${Extend_Exec}${Extend_Exec_Args}'" >> "${Snmpd_Conf}"
-                if [ $? -eq 0 ]; then
-                    echo "+ extend ${Extend_Name} installed."
-                else
-                    echo "+ extend ${Extend_Name} failed to install."
-                fi
-            fi
-        else
-            echo "+ extend ${Extend_Name} returns success."
-        fi
-    fi
-    unset -v Extend_Exec_Args Extend_Basename Extend_Name Extend_RC
-done <<< "$(find "${Dirname}" -name "extend-*" | egrep -ve 'extend-include|extend-info|conf$|example|cfg$' | xargs -r ls -1)"
-
+exit 1
